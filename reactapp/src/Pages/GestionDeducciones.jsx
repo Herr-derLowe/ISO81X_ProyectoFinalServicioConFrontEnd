3c87ac43726b6a4bcb381b2933495740ecdd424f
Reluhcs <126292478+Herr-derLowe@users.noreply.github.com>
2023-03-05 22:58:55 -0400

    PreInicio Deducciones

--- reactapp/src/Pages/GestionDeducciones.jsx
+++ reactapp/src/Pages/GestionDeducciones.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content';
 
 export class GestionDeducciones extends Component {
     render() {
+        const MySwal = withReactContent(Swal)
+        MySwal.fire({
+            title: <strong>Deducci&oacute;n Insertada!</strong>,
+            icon: 'success'
+        });
         return (
+            
             <h1 className="text-center">Bienvenido/a a la lista de Departamentos de Walmart Inc.</h1>
         )
     }
