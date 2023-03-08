df9a96e95124db978e96c0321d4b206c7485aba1
Eduardo Cruz-Betances <eduardo.cruz@ikeasi.com>
2023-03-07 21:14:49 -0300

    wip 2 transactions

--- reactapp/src/Pages/Transacciones.jsx
+++ reactapp/src/Pages/Transacciones.jsx
@@ -32,6 +32,7 @@ export class Transacciones extends React.Component {
         axios.get(variables.API_URL + 'Transacciones/GetTransacciones')
             .then(res => {
                 const data = res.data;
+
                 this.setState({ transacciones: data });
             })
     }
@@ -51,6 +52,14 @@ export class Transacciones extends React.Component {
                 this.setState({ deducciones: data });
             })
     }
+
+    getEmpleado() {
+        axios.get(variables.API_URL + 'Empleados/GetEmpleadoById/6400d6af2a54a5276703b55e')
+            .then(res => {
+                const data = res.data;
+            })
+    }
+
     componentDidMount() {
 
         this.refreshList();
@@ -160,15 +169,33 @@ export class Transacciones extends React.Component {
                                                         {transacciones.map(t =>
                                                             <tr key={t.id}>
                                                                 <td>{t.id}</td>
-                                                                <td>{t.empleado_id}</td>
-                                                                <td>{t.ingresoDeduccion_id}</td>
+                                                                <td>Rafael Leonidas Trujillo</td>
+                                                                <td>ARS</td>
                                                                 <td>{t.tipoTransaccion}</td>
                                                                 <td>{t.fechaTransaccion}</td>
                                                                 <td>{t.montoTransaccion}</td>
                                                                 <td>{t.estadoTransaccion}</td>
-                                                                <td></td>
-                                                            </tr>
+                                                                <td>
+                                                                    <button type="button"
+                                                                        className="btn btn-primary mr-1"
+                                                                        data-bs-toggle="modal"
+                                                                        data-bs-target="#exampleModal"
+                                                                        onClick={() => this.editClick(ingres)}>
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                                        </svg> Editar
+                                                                    </button>
 
+                                                                    <button type="button"
+                                                                        className="btn btn-danger mr-1"
+                                                                        onClick={() => this.deleteClick(ingres.id)}>
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                                                        </svg> Eliminar
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
                                                         )}
 
                                                     </tbody>
