cb445cefc0e68671b1282562a188b6ae78e957ef
Eduardo Cruz-Betances <eduardo.cruz@ikeasi.com>
2023-03-10 16:42:40 -0300

    done

--- reactapp/src/Pages/Transacciones.jsx
+++ reactapp/src/Pages/Transacciones.jsx
@@ -4,6 +4,9 @@ import withReactContent from 'sweetalert2-react-content';
 import { variables } from '../Components/Variables';
 import axios from 'axios';
 
+const cache_ingresos = {};
+const cache_deducciones = {};
+const cache_empleados = {};
 
 export class Transacciones extends React.Component {
     constructor(props) {
@@ -12,6 +15,10 @@ export class Transacciones extends React.Component {
             transacciones: [],
             empleados: [],
             deducciones: [],
+            ingresos: [],
+            current_empleado: {},
+            current_ingreso: {},
+            current_deduccion: {},
             id: "",
             modalTitle: "",
 
@@ -28,7 +35,9 @@ export class Transacciones extends React.Component {
     }
     refreshList() {
 
-
+        this.getEmpleados();
+        this.getDeducciones();
+        this.getIngresos();
         axios.get(variables.API_URL + 'Transacciones/GetTransacciones')
             .then(res => {
                 const data = res.data;
@@ -41,7 +50,7 @@ export class Transacciones extends React.Component {
         axios.get(variables.API_URL + 'Empleados/GetEmpleados')
             .then(res => {
                 const data = res.data;
-                this.setState({ empleados: data });
+                this.setState({ empleados: data, empleado: data[0].id });
             })
     }
 
@@ -49,15 +58,70 @@ export class Transacciones extends React.Component {
         axios.get(variables.API_URL + 'TiposDeducciones/GetTiposDeducciones')
             .then(res => {
                 const data = res.data;
+
                 this.setState({ deducciones: data });
             })
     }
 
-    getEmpleado() {
-        axios.get(variables.API_URL + 'Empleados/GetEmpleadoById/6400d6af2a54a5276703b55e')
+    getIngresos() {
+        axios.get(variables.API_URL + 'TiposIngresos/GetTiposIngresos')
             .then(res => {
                 const data = res.data;
+
+                this.setState({ ingresos: data, deduccion: data[0].id });
             })
+    }
+
+    async getEmpleado(id) {
+        if (!cache_empleados[id]) {
+            const res = await axios.get(variables.API_URL + 'Empleados/GetEmpleadoById/' + id);
+            const data = res.data;
+            this.setState({ current_empleado: data });
+            cache_empleados[id] = data;
+            console.log(data.nombreEmpleado);
+            return data.nombreEmpleado;
+
+        } else {
+            let data = cache_empleados[id];
+            return data.nombreEmpleado;
+            // this.setState({ current_empleado: data });
+        }
+
+    }
+
+    getDeduccion(id) {
+        if (!cache_deducciones[id]) {
+            axios.get(variables.API_URL + 'TiposDeducciones/GetTipoDeduccionById/' + id)
+                .then(res => {
+                    const data = res.data;
+                    this.setState({ current_deduccion: data });
+                    cache_deducciones[id] = data;
+                })
+        } else {
+            // this.setState({ current_deduccion: cache_deducciones[id] });
+        }
+    }
+
+    getIngreso(id) {
+        if (!cache_ingresos[id]) {
+            axios.get(variables.API_URL + 'TiposIngresos/GetTipoIngresoById/' + id)
+                .then(res => {
+                    const data = res.data;
+                    this.setState({ current_ingreso: data });
+                    cache_ingresos[id] = data;
+                })
+        } else {
+            // this.setState({ current_ingreso: cache_ingresos[id] });
+        }
+    }
+
+    getDeducIng(t) {
+        if (t.tipoTransaccion == "INGRESO") {
+            return this.state.ingresos.find(e => e.id == t.ingresoDeduccion_id).nombreIngreso
+
+        } else {
+            return this.state.deducciones.find(e => e.id == t.ingresoDeduccion_id).nombreDeduccion
+        }
     }
 
     componentDidMount() {
@@ -68,19 +132,34 @@ export class Transacciones extends React.Component {
     addClick() {
         this.getEmpleados();
         this.getDeducciones();
+        this.getIngresos();
         this.setState({
-            modalTitle: "Agregar Tipo de ingreso",
-            claveIngreso: "",
-            nombreIngreso: "",
-            dependeSalarioI: true,
-            estadoIngreso: "ACTIVO",
+            modalTitle: "Agregar Transaccion",
+            tipoTransaccion: "INGRESO",
+            fechaTransaccion: new Date(),
+            montoTransaccion: 0,
+            estadoTransaccion: 'PENDIENTE APROBACION'
         });
     }
-
+    editClick(t) {
+        this.getEmpleados();
+        this.getDeducciones();
+        this.getIngresos();
+        this.setState({
+            modalTitle: "Editar Tipo de Ingreso",
+            id: t.id,
+            empleado: t.empleado_id,
+            tipoTransaccion: t.tipoTransaccion,
+            fechaTransaccion: t.fechaTransaccion,
+            montoTransaccion: t.montoTransaccion,
+            estadoTransaccion: t.estadoTransaccion
+        });
+    }
     changeDeduccion = (e) => {
         this.setState({ deduccion: e.target.value });
     }
     changeEmpleado = (e) => {
+        console.log("INIT ", e);
         this.setState({ empleado: e.target.value });
     }
     changeMonto = (e) => {
@@ -88,6 +167,14 @@ export class Transacciones extends React.Component {
     }
     changeTipo = (e) => {
         this.setState({ tipoTransaccion: e.target.value });
+        if (e.target.value == "INGRESO") {
+            this.setState({ deduccion: this.state.ingresos[0].id });
+
+        } else {
+            this.setState({ deduccion: this.state.deducciones[0].id });
+
+        }
+
     }
     createClick() {
         const MySwal = withReactContent(Swal)
@@ -97,10 +184,10 @@ export class Transacciones extends React.Component {
         axios.post(variables.API_URL + 'Transacciones/PostAddTransaccion', {
             empleado_id: this.state.empleado,
             tipoTransaccion: this.state.tipoTransaccion,
-            ingresoDeduccion_id: '',
-            fechaTransaccion: '',
+            ingresoDeduccion_id: this.state.deduccion,
+            fechaTransaccion: new Date(),
             montoTransaccion: this.state.montoTransaccion,
-            estadoTransaccion: ''
+            estadoTransaccion: 'PENDIENTE APROBACION'
         })
             .then(() => {
                 MySwal.fire({
@@ -117,11 +204,79 @@ export class Transacciones extends React.Component {
             })
     }
 
+    updateClick() {
+
+        const MySwal = withReactContent(Swal)
+
+
+        axios.put(variables.API_URL + 'Transacciones/UpdateTransaccion/' + this.state.id, {
+            empleado_id: this.state.empleado,
+            tipoTransaccion: this.state.tipoTransaccion,
+            ingresoDeduccion_id: this.state.deduccion,
+            fechaTransaccion: new Date(),
+            montoTransaccion: this.state.montoTransaccion,
+            estadoTransaccion: 'PENDIENTE APROBACION'
+        })
+            .then(() => {
+                MySwal.fire({
+                    title: <strong>Ingreso Actualizado!</strong>,
+                    icon: 'success',
+                    didClose: () => {
+                        this.refreshList()
+                    }
+                });
+                this.refreshList();
+            }, (error) => {
+                MySwal.fire({
+                    title: <strong>Error: No se pudo actualizar el ingreso...</strong>,
+                    icon: 'error'
+                });
+                this.refreshList();
+                console.log(error);
+            })
+    }
+
+    deleteClick(id) {
+
+        const MySwal = withReactContent(Swal)
+        MySwal.fire({
+            title: 'Estas seguro?',
+            text: "No se puede revertir esta operacion!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminalo!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete(variables.API_URL + 'Transacciones/DeleteTransaccion/' + id)
+                    .then(() => {
+                        MySwal.fire({
+                            title: 'Transaccion Eliminada!',
+                            icon: 'success'
+                        })
+                        this.refreshList();
+                    }, (error) => {
+                        MySwal.fire({
+                            title: <strong>Error: No se pudo eliminar el ingreso...</strong>,
+                            icon: 'error'
+                        });
+                        console.log(error);
+                    })
+
+            }
+        })
+    }
+
     render() {
         const {
             transacciones,
             empleados,
+            current_empleado,
+            current_deduccion,
+            current_ingreso,
             deducciones,
+            ingresos,
             modalTitle,
             id,
             empleado_id,
@@ -132,6 +287,39 @@ export class Transacciones extends React.Component {
             estadoTransaccion
 
         } = this.state;
+        
+        const renderDeduccion = () => {
+            if (tipoTransaccion == "DEDUCCION") {
+                return (<>
+                    <label htmlFor="inputDeduccion" className="form-label">Tipo de Deduccion</label>
+                    <select
+                        className="form-select"
+                        id="inputDeduccion"
+                        onChange={this.changeDeduccion}
+                    >
+                        {deducciones.map(e =>
+                            <option value={e.id}>{e.nombreDeduccion}</option>
+
+                        )}
+                    </select>
+                </>)
+
+            } else {
+                return (<>
+                    <label htmlFor="inputDeduccion" className="form-label">Tipo de Ingreso</label>
+                    <select
+                        className="form-select"
+                        id="inputDeduccion"
+                        onChange={this.changeDeduccion}
+                    >
+                        {ingresos.map(e =>
+                            <option value={e.id}>{e.nombreIngreso}</option>
+
+                        )}
+                    </select>
+                </>)
+            }
+        }
         return (
             <div className="main_container">
                 <div className="container">
@@ -157,7 +345,7 @@ export class Transacciones extends React.Component {
                                                         <tr>
                                                             <th>ID</th>
                                                             <th>Empleado</th>
-                                                            <th>Deduccion</th>
+                                                            <th>Deduccion/Ingreso</th>
                                                             <th>Tipo</th>
                                                             <th>Fecha</th>
                                                             <th>Monto</th>
@@ -169,8 +357,10 @@ export class Transacciones extends React.Component {
                                                         {transacciones.map(t =>
                                                             <tr key={t.id}>
                                                                 <td>{t.id}</td>
-                                                                <td>Rafael Leonidas Trujillo</td>
-                                                                <td>ARS</td>
+                                                                <td>{this.state.empleados.find(e => e.id == t.empleado_id).nombreEmpleado}</td>
+                                                                <td>{
+                                                                    this.getDeducIng(t)
+                                                                }</td>
                                                                 <td>{t.tipoTransaccion}</td>
                                                                 <td>{t.fechaTransaccion}</td>
                                                                 <td>{t.montoTransaccion}</td>
@@ -228,22 +418,6 @@ export class Transacciones extends React.Component {
                                                                                 )}
                                                                             </select>
                                                                         </div>
-
-                                                                        <div className="mb-3">
-                                                                            <label htmlFor="inputDeduccion" className="form-label">Tipo de Deduccion</label>
-
-                                                                            <select
-                                                                                className="form-select"
-                                                                                id="inputDeduccion"
-                                                                                onChange={this.changeDeduccion}
-                                                                            >
-                                                                                {deducciones.map(e =>
-                                                                                    <option value={e.id}>{e.nombreDeduccion}</option>
-
-                                                                                )}
-                                                                            </select>
-                                                                        </div>
-
                                                                         <div className="input-group mb-3">
                                                                             <label className="input-group-text" htmlFor="inputTipo">Tipo de Transaccion</label>
                                                                             <select
@@ -253,9 +427,14 @@ export class Transacciones extends React.Component {
                                                                                 onChange={this.changeTipo}
                                                                             >
                                                                                 <option value="INGRESO">Ingreso</option>
-                                                                                <option value="GASTO">Gasto</option>
+                                                                                <option value="DEDUCCION">Deduccion</option>
                                                                             </select>
                                                                         </div>
+
+                                                                        <div className="mb-3">
+                                                                            {renderDeduccion()}
+                                                                        </div>
+
                                                                         <div className="mb-3">
                                                                             <label htmlFor="inputMonto" className="form-label">Monto</label>
                                                                             <input
@@ -274,19 +453,18 @@ export class Transacciones extends React.Component {
                                                                         <br />
                                                                     </div>
                                                                 </div>
-
                                                                 {id === "" ?
                                                                     <button type="button"
                                                                         className="btn btn-primary float-start"
                                                                         onClick={() => this.createClick()}
-                                                                    >Crear Tipo Deducci&oacute;n</button>
+                                                                    >Crear Transaccion</button>
                                                                     : null}
 
                                                                 {id !== "" ?
                                                                     <button type="button"
                                                                         className="btn btn-primary float-start"
                                                                         onClick={() => this.updateClick()}
-                                                                    >Actualizar Tipo Deducci&oacute;n</button>
+                                                                    >Actualizar Transaccion</button>
                                                                     : null}
                                                             </div>
 
