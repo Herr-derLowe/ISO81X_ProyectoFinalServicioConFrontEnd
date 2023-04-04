eb7fbe67dd806ffcab005b1cafe6c401dac0257a
Reluhcs <126292478+Herr-derLowe@users.noreply.github.com>
2023-04-03 21:45:31 -0400

    Correcion sendVerify

--- reactapp/src/Components/PrivateRoute/PrivateRoute.jsx
+++ reactapp/src/Components/PrivateRoute/PrivateRoute.jsx
@@ -4,43 +4,48 @@ import { useEffect, useState } from "react";
 import { apiUrl } from "../../context/apiUrl";
 
 export function PrivateRoute() {
-  const [isAuth, setIsAuth] = useState(null);
-  const token = JSON.parse(sessionStorage.getItem("token"));
+    const [isAuth, setIsAuth] = useState(null);
+    const token = sessionStorage.getItem("token");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const sendVerify = async () => {
-    await axios
-      .get(apiUrl + "Authentication/verifyjwt/" + token)
-      .then((data) => {
-        setIsAuth(true);
-      })
-      .catch((err) => {
-        navigate("/");
-      });
-  };
+    const sendVerify = async () => {
+        await axios
+            .get(apiUrl + "Authentication/verifyjwt/" + token)
+            .then(res => {
+                const data = res.data;
+                if (data == true) {
+                    setIsAuth(true);
+                } else {
+                    setIsAuth(false)
+                }
+            })
+            .catch((err) => {
+                navigate("/");
+            });
+    };
 
-  useEffect(() => {
-    sendVerify();
-  }, []);
+    useEffect(() => {
+        sendVerify();
+    }, []);
 
-  if (!isAuth) {
-    return (
-      <div
-        style={{
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "3rem",
-          fontWeight: "600",
-        }}
-      >
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
+    if (!isAuth) {
+        return (
+            <div
+                style={{
+                    width: "100%",
+                    height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "3rem",
+                    fontWeight: "600",
+                }}
+            >
+                <h1>Loading (Please Login)...</h1>
+            </div>
+        );
+    }
 
-  return <Outlet />;
+    return <Outlet />;
 }
--- reactapp/src/Pages/login/Login.jsx
+++ reactapp/src/Pages/login/Login.jsx
@@ -7,73 +7,74 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export function Login() {
-  //get the data of inputs
-  const [dataLogin, setDataLogin] = useState({
-    username: null,
-    password: null,
-  });
-
-  const navigate = useNavigate();
-
-  // we need create a function for get the data of the inputs
-  const getData = (e) => {
-    setDataLogin({
-      ...dataLogin,
-      [e.target.name]: e.target.value,
+    //get the data of inputs
+    const [dataLogin, setDataLogin] = useState({
+        username: null,
+        password: null,
     });
-  };
 
-  const submitData = async (e) => {
-    e.preventDefault();
+    const navigate = useNavigate();
 
-    await axios
-      .post(apiUrl + "Authentication/Login", {
-        username: dataLogin.username,
-        password: dataLogin.password,
-      })
-      .then((x) => {
-        console.log(x.data);
-        /* localStorage.setItem("token", x.data.token); */
-        navigate("/home");
-      });
-  };
+    // we need create a function for get the data of the inputs
+    const getData = (e) => {
+        setDataLogin({
+            ...dataLogin,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-  return (
-    <div className={style.body}>
-      <section>
-        <h1>Sistema de nomina</h1>
+    const submitData = async (e) => {
+        e.preventDefault();
 
-        <form onSubmit={submitData}>
-          <div className={style.input}>
-            <span>
-              Usuaurio <strong>*</strong>
-            </span>
+        await axios
+            .post(apiUrl + "Authentication/Login", {
+                username: dataLogin.username,
+                password: dataLogin.password,
+            })
+            .then((x) => {
+                console.log(x.data);
+                /* localStorage.setItem("token", x.data.token); */
+                sessionStorage.setItem("token", x.data.token);
+                navigate("/home");
+            });
+    };
 
-            <input type="text" name="username" required onChange={getData} />
-          </div>
+    return (
+        <div className={style.body}>
+            <section>
+                <h1>Sistema de nomina</h1>
 
-          <div className={style.input}>
-            <span>
-              Contraseña <strong>*</strong>
-            </span>
+                <form onSubmit={submitData}>
+                    <div className={style.input}>
+                        <span>
+                            Usuario <strong>*</strong>
+                        </span>
 
-            <input
-              type="password"
-              name="password"
-              required
-              onChange={getData}
-            />
-          </div>
+                        <input type="text" name="username" required onChange={getData} />
+                    </div>
 
-          <div className={style.send}>
-            <button>Login</button>
-          </div>
-        </form>
+                    <div className={style.input}>
+                        <span>
+                            Contraseña <strong>*</strong>
+                        </span>
 
-        <footer className={style.footer}>
-          <h3>Copyright @Dann, Inc. 2023</h3>
-        </footer>
-      </section>
-    </div>
-  );
+                        <input
+                            type="password"
+                            name="password"
+                            required
+                            onChange={getData}
+                        />
+                    </div>
+
+                    <div className={style.send}>
+                        <button>Login</button>
+                    </div>
+                </form>
+
+                <footer className={style.footer}>
+                    <h3>Copyright @Dann, Inc. 2023</h3>
+                </footer>
+            </section>
+        </div>
+    );
 }
